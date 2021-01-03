52c31488bfb945030531f0c65dcbc48df808f416
Yew Kang Wei <yewyew6933@gmail.com>
2021-01-04 07:58:56 +0800

    performance optimize test

--- src/components/HomeComponents/HeroSection.jsx
+++ src/components/HomeComponents/HeroSection.jsx
@@ -23,14 +23,14 @@ const HeroSection = () => {
       <div className="VideoContainer | all-center h-100 position-absolute overflow-hidden">
         {/* blurred video */}
         <video
-          className="BlurredVideo | h-100 position-absolute"
+          className="BlurredVideo | d-md-none d-block h-100 position-absolute"
           loop
           playsInline
           autoPlay
           muted
           preload="auto"
           poster="https://instagram.fkul14-1.fna.fbcdn.net/v/t51.2885-15/e35/s1080x1080/88131322_242863696739852_8645542826988865499_n.jpg?_nc_ht=instagram.fkul14-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=oxcWcFDutwkAX_zyVZR&tp=1&oh=acf436f5d136df7708b79439a064b338&oe=5FF3472B"
-          src="hero_video_small.mp4"
+          src="media/compressed-trim.mp4"
           type="video/mp4"
         ></video>
         <div className="BlurredBackdrop | h-100 w-100 position-absolute"></div>
@@ -44,8 +44,7 @@ const HeroSection = () => {
           muted
           preload="auto"
           poster="https://instagram.fkul14-1.fna.fbcdn.net/v/t51.2885-15/e35/s1080x1080/88131322_242863696739852_8645542826988865499_n.jpg?_nc_ht=instagram.fkul14-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=oxcWcFDutwkAX_zyVZR&tp=1&oh=acf436f5d136df7708b79439a064b338&oe=5FF3472B"
-          src="hero_video_small.mp4"
-          // src="https://scontent-iad3-1.cdninstagram.com/v/t50.2886-16/87761858_235555977470480_8244806077495858175_n.mp4?efg=eyJ2ZW5jb2RlX3RhZyI6InZ0c192b2RfdXJsZ2VuLjk2MC5mZWVkLmRlZmF1bHQiLCJxZV9ncm91cHMiOiJbXCJpZ193ZWJfZGVsaXZlcnlfdnRzX290ZlwiXSJ9&_nc_ht=scontent-iad3-1.cdninstagram.com&_nc_cat=108&_nc_ohc=oAvMwavAK2QAX_s81of&vs=17917935946377672_1444852449&_nc_vs=HBksFQAYJEdNSWpPd1VRb2x5bVBOWUFBUDlUSDlOeGIydHlia1lMQUFBRhUAAsgBABUAGCRHSVhYVFFXUVBtMUJQdXdCQUszckJEdGozRVlrYmtZTEFBQUYVAgLIAQAoABgAGwAVAAAmkNX1xY6R1D8VAigCQzMsF0A7CHKwIMScGBJkYXNoX2Jhc2VsaW5lXzFfdjERAHXqBwA%3D&_nc_rid=be7d749e4f&oe=5FF3CCF1&oh=93c67ffb6c502756562172a027968904&dl=1"
+          src="media/trimmed-origin.mp4"
           type="video/mp4"
         ></video>
       </div>
