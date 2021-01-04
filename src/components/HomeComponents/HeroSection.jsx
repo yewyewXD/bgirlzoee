a501abdccfe302fed6db1a8252a8909dec7f0816
Yew Kang Wei <yewyew6933@gmail.com>
2021-01-04 08:51:16 +0800

    improve default

--- src/components/HomeComponents/HeroSection.jsx
+++ src/components/HomeComponents/HeroSection.jsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
   return (
     <section className="HeroSection">
       {/* caption */}
-      <div className="Caption | h-100 w-100 all-center text-white position-absolute">
+      <div className="Caption | h-100 w-100 all-center position-absolute">
         <div className="container">
           <div className="Caption__Title | heading mb-3">Zoée Hablot</div>
           <div>Breakdancer, photographer, artist, designer</div>
