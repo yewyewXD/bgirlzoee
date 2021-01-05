15a3303fb51dd5f568b1f759e08fe4599a0a49bd
Yew Kang Wei <yewyew6933@gmail.com>
2021-01-05 15:08:05 +0800

    install react-reveal, add more interests

--- src/components/HomeComponents/InterestsSection.jsx
+++ src/components/HomeComponents/InterestsSection.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 
 const InterestsSection = () => {
+  const interests = [
+    {
+      percentage: 100,
+      text: "K-pop",
+    },
+    {
+      percentage: 100,
+      text: "Breakdance",
+    },
+    {
+      percentage: 70,
+      text: "Photography",
+    },
+    {
+      percentage: 60,
+      text: "Singing & Rapping",
+    },
+  ];
+
   return (
     <section className="InterestsSection | pt-5" id="interests">
       <div className="container my-sm-5">
@@ -11,31 +30,37 @@ const InterestsSection = () => {
         </div>
 
         {/* content */}
-        <div className="CardContainer | all-center text-center position-relative overflow-hidden">
-          <div>
-            <div className="PercentContainer | position-relative">
-              <svg className="position-relative">
-                <circle cx="70" cy="70" r="70" />
-                <circle
-                  className="w-100 h-100"
-                  style={{
-                    strokeDashoffset: 440 - (440 * 90) / 100,
-                  }}
-                  cx="70"
-                  cy="70"
-                  r="70"
-                />
-              </svg>
-              <div className="Percent | position-absolute all-center w-100 h-100">
-                <h2 className="m-0">
-                  90<span>%</span>
-                </h2>
+        {interests.map((interest) => (
+          <div
+            className="InterestCard | all-center text-center position-relative overflow-hidden"
+            key={interest.text}
+          >
+            <div>
+              <div className="PercentContainer | position-relative">
+                <svg className="position-relative">
+                  <circle cx="70" cy="70" r="70" />
+                  <circle
+                    className="w-100 h-100"
+                    style={{
+                      strokeDashoffset: 440 - (440 * interest.percentage) / 100,
+                    }}
+                    cx="70"
+                    cy="70"
+                    r="70"
+                  />
+                </svg>
+                <div className="Percent | position-absolute all-center w-100 h-100">
+                  <h2 className="m-0">
+                    {interest.percentage}
+                    <span>%</span>
+                  </h2>
+                </div>
               </div>
-            </div>
 
-            <h4 className="mt-3">interest 1</h4>
+              <h4 className="mt-3">{interest.text}</h4>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </section>
   );
